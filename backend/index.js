8c66fe4d062f8ac1a2feb44ac362f32aba3126a7
SweetDreamEng <sweetdream.eng0614@gmail.com>
2021-01-18 04:05:37 +0000

    backend/install-cors

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const { init } = require('./db')
-const routes = require('./routes')
+const express = require("express");
+const bodyParser = require("body-parser");
+const { init } = require("./db");
+const routes = require("./routes");
+const cors = require("cors");
 
-const app = express()
-app.use(bodyParser.json())
-app.use(routes)
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(routes);
 
 init().then(() => {
-  console.log('starting server on port 3000')
-  app.listen(3000)
-})
+  console.log("starting server on port 3000");
+  app.listen(3000);
+});
